29568bf92ba1741c0c93aa85c1139bd897dd025f
EnduringElf <69624373+EnduringElf@users.noreply.github.com>
2022-09-08 21:00:14 +0200

    soil added

--- OpenGLApp/OpenGLApp.cpp
+++ OpenGLApp/OpenGLApp.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include "Shader.h"
 
+#include "SOIL2.h"
+
 //GLFW
 #define GLEW_STATIC
 #include <GL/glew.h>
@@ -12,22 +14,7 @@
 //windows diminsions
 const GLint WIDTH = 800, HEIGHT = 600;
 
-//shaders
-//vertex
-//const GLchar* VertexshaderSource = "#version 330 core \n"
-//"layout (location = 0) in vec3 position;\n"
-//"void main()\n"
-//"{\n"
-//"gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
-//"}\0";
 
-//fragment
-//const GLchar* fragmentShaderSource = "#version 330 core\n"
-//"out vec4 color;\n"
-//"void main()\n"
-//"{\n"
-//"color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-//"}\n\0";
 
 
 int main()
@@ -69,57 +56,10 @@ int main()
 	// Define the viewport dimensions
 	glViewport(0, 0, screenWidth, screenHeight);
 
-	//old way of adding shaders
-	{
-		////build and compile shaders
-	////vertex shader
-	//GLuint vertexshader = glCreateShader(GL_VERTEX_SHADER);
-	//glShaderSource(vertexshader, 1, &VertexshaderSource, NULL);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-	//glCompileShader(vertexshader);
-	////check for comile time errors
-	//GLint success;
-	//GLchar infolog[512];
 
-	//glGetShaderiv(vertexshader, GL_COMPILE_STATUS, &success);
-
-	//if (!success) {
-	//	glGetShaderInfoLog(vertexshader, 512, NULL, infolog);
-	//	std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infolog << std::endl;
-	//}
-
-	//// Fragment shader
-	//GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	//glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	//glCompileShader(fragmentShader);
-
-	//// Check for compile time errors
-	//glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-
-	//if (!success)
-	//{
-	//	glGetShaderInfoLog(fragmentShader, 512, NULL, infolog);
-	//	std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infolog << std::endl;
-	//}
-
-	//// Link shaders
-	//GLuint shaderProgram = glCreateProgram();
-	//glAttachShader(shaderProgram, vertexshader);
-	//glAttachShader(shaderProgram, fragmentShader);
-	//glLinkProgram(shaderProgram);
-
-	//// Check for linking errors
-	//glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-
-	//if (!success)
-	//{
-	//	glGetProgramInfoLog(shaderProgram, 512, NULL, infolog);
-	//	std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infolog << std::endl;
-	//}
-
-	//glDeleteShader(vertexshader);
-	//glDeleteShader(fragmentShader);
-	}
 	
 
 	//build and compile our shader
@@ -128,31 +68,70 @@ int main()
 	// Set up vertex data (and buffer(s)) and attribute pointers
 	GLfloat vertices[] =
 	{
-		//positions				//colors
-		-0.5f, -0.5f, 0.0f,		1.0f,0.0f,0.0f,		// bottom right
-		0.5f, -0.5f, 0.0f,		0.0f,1.0f,0.0f,		// bottom left
-		0.0f,  0.5f, 0.0f,		0.0f,0.0f,1.0f		// Top
+		// Positions          // Colors           // Texture Coords
+		0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // Top Right
+		0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // Bottom Right
+		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // Bottom Left
+		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // Top Left
+	};
+	GLuint indices[] =
+	{  // Note that we start from 0!
+		0, 1, 3, // First Triangle
+		1, 2, 3  // Second Triangle
 	};
 
-	GLuint VBO, VAO;
+	GLuint VBO, VAO, EBO; 
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
-	// Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
+	glGenBuffers(1, &EBO);
+
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	
-	//position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// Position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
-	//color attribute
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	// Color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
 	glEnableVertexAttribArray(1);
+	// Texture Coordinate attribute
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(2);
 
-	//glBindBuffer(GL_ARRAY_BUFFER, 0); // Note that this is allowed, the call to glVertexAttribPointer registered VBO as the currently bound vertex buffer object so afterwards we can safely unbind
+	glBindVertexArray(0); // Unbind VAO
 
-	glBindVertexArray(0); // Unbind VAO (it's always a good thing to unbind any buffer/array to prevent strange bugs)
+	// Load and create a texture
+	GLuint texture;
+
+	int width, height;
+
+	// ===================
+  // Texture
+  // ===================
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	// Set our texture parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	// Set texture filtering
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	// Load, create texture and generate mipmaps
+	unsigned char* image = SOIL_load_image("res/images/image1.jpg", &width, &height, 0, SOIL_LOAD_RGBA);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+	glGenerateMipmap(GL_TEXTURE_2D);
+	SOIL_free_image_data(image);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+
 
 	// Game loop
 	while (!glfwWindowShouldClose(window))
@@ -165,11 +144,15 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		// Draw our first triangle
+		// Draw the triangle
 		ourShader.Use();
-		//glUseProgram(shaderProgram);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture);
+		glUniform1i(glGetUniformLocation(ourShader.Program, "ourTexture"), 0);
+
+		// Draw container
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
 
 		// Swap the screen buffers
@@ -179,6 +162,7 @@ int main()
 	// Properly de-allocate all resources once they've outlived their purpose
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 
 	// Terminate GLFW, clearing any resources allocated by GLFW.
 	glfwTerminate();
